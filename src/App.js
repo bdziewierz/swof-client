c7fbcac33990239776b4f7580b2c3e2da0a36b1f
Bogdan Dziewierz <bogdan@digital.crafts.agency>
2017-11-20 18:55:07 +0000

    Calling BAU service to get full list of engineers and todays BAU

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import moment from 'moment';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            engineers: [],
+            baus: []
+        };
+    }
+
+    componentDidMount(){
+        // Call an API endpoint and get BAU for today's date.
+        let now = moment();
+        fetch(`https://95anp0ckjc.execute-api.eu-west-1.amazonaws.com/dev/bau/` + now.format('YYYY-MM-DD'))
+            .then( result => result.json() )
+            .then( bauData => this.setState(bauData) )
+    }
+
+    render() {
+        const engineers = this.state.engineers.map((engineer) =>
+            <li>{engineer}</li>
+        );
+
+        const baus = this.state.baus.map((bauEngineer) =>
+            <li>{bauEngineer}</li>
+        );
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Support Wheel of Fate</h1>
+                </header>
+                <p className="App-intro">
+                    <ul>{engineers}</ul>
+                    <ul>{baus}</ul>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
